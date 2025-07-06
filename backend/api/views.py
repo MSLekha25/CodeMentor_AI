7745fde17d2b45e56166c58adc43eefc345937b1
Mucharla Sreelekha <sreelekhaoff@gmail.com>
2025-07-06 13:31:32 +0530

    updated keys
    dotenv

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,15 +8,15 @@ import os
 from django.utils.crypto import get_random_string
 from django.db.models import Q
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 
-AZURE_OPENAI_API_KEY="8N18yr7G9S4EFSXckoa9m9HcB1KuFIyeXUA1mlHACPjH5gH0b69lJQQJ99BFACHYHv6XJ3w3AAABACOGvIRK"
-AZURE_OPENAI_ENDPOINT="https://aiseccureform.openai.azure.com/"
-AZURE_OPENAI_DEPLOYMENT="gpt-4o"
-AZURE_OPENAI_API_VERSION="2025-01-01-preview"
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+AZURE_OPENAI_API_VERSION = "2025-01-01-preview"
 
 from .models import SignupLog, ChatHistory
 
